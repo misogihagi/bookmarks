c8149dbaaf472e1a67df939bdad890522b9c30c1
misogihagi <47350059+misogihagi@users.noreply.github.com>
2025-03-17 18:18:21 +0900

    Update yaml-to-bookmarks.js

--- scripts/yaml-to-bookmarks.js
+++ scripts/yaml-to-bookmarks.js
@@ -1,39 +1,39 @@
+import fs from 'fs';
 import yaml from 'js-yaml';
+
+function generateBookmarks(items) {
+    let html = '';
+    if (Array.isArray(items)) {
+        for (const item of items) {
+            html += generateBookmarks(item);
+        }
+    } else if (typeof items === 'object' && items !== null) {
+        for (const [title, item] of Object.entries(items)) {
+            if (typeof item === 'string') {
+                html += `\n<DT><A HREF="${item}">${title}</A>`;
+            } else {
+                html += `\n<DT><H3>${title}</H3>`;
+                html += `\n<DL><p>`;
+                html += generateBookmarks(item);
+                html += `\n</DL><p>`;
+            }
+        }
+    }
+    return html;
+}
 
 function yamlToBookmarks(yamlString) {
     const jsonData = yaml.load(yamlString);
     
-    function generateBookmarks(items) {
-        let html = '';
-        if (Array.isArray(items)) {
-            for (const item of items) {
-                if (typeof item === 'object' && item !== null) {
-                    for (const [folderName, links] of Object.entries(item)) {
-                        html += `\n<DT><H3>${folderName}</H3>`;
-                        html += `\n<DL><p>`;
-                        html += generateBookmarks(links);
-                        html += `\n</DL><p>`;
-                    }
-                }
-            }
-        } else if (typeof items === 'object' && items !== null) {
-            for (const [title, url] of Object.entries(items)) {
-                html += `\n<DT><A HREF="${url}">${title}</A>`;
-            }
-        }
-        return html;
-    }
-    
     let bookmarksHtml = `<!DOCTYPE NETSCAPE-Bookmark-file-1>\n<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">\n<TITLE>Bookmarks</TITLE>\n<H1>Bookmarks</H1>\n<DL><p>`;
-    bookmarksHtml += generateBookmarks(jsonData);
+    bookmarksHtml += generateBookmarks(jsonData['Imported']);
     bookmarksHtml += `\n</DL><p>`;
     
     return bookmarksHtml;
 }
 
-// Example usage:
-const yamlInput = `
-- import:
+const sampleYamlInput = `
+Imported:
     - Mozilla Firefox:
         Get Help: https://support.mozilla.org/en-US/products/firefox
         Customize Firefox: >-
@@ -42,5 +42,11 @@ const yamlInput = `
         About Us: https://www.mozilla.org/en-US/about/
 `;
 
-const bookmarksHtml = yamlToBookmarks(yamlInput);
-console.log(bookmarksHtml);
+if (!process.argv[2]) {
+    console.log(`File Argument not found. Give me bookmarks yaml like:${sampleYamlInput}`)
+} else {
+    const bookmarksHtml = yamlToBookmarks(fs.readFileSync(process.argv[2], 'utf-8'));
+
+    console.log(bookmarksHtml);
+}
+
